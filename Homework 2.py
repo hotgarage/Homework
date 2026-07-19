d56f6ce91b80344b08ae2aeb22d4a67358cbdbaa
SergeySad <281275005+hotgarage@users.noreply.github.com>
2026-07-20 00:31:35 +0300

    homework 2

---
+++ Homework 2.py
@@ -0,0 +1,62 @@
+# 1
+# operation_name1 = "“Квадрат числа“"
+# print (operation_name1)
+# variable1 = int (input( "Введіть число:") )
+# result = variable1 * variable1
+# operation_name11  = "Квадрат числа:"
+# print (operation_name11 , result)
+# 2
+# operation_name2 = "“Середнє трьох чисел“"
+# print (operation_name2)
+# variable11 = int(input("Введіть перше число: "))
+# variable12 = int(input("Введіть друге число: "))
+# variable13 = int(input("Введіть третье число: "))
+# result = (variable11 + variable12 + variable13)/3
+# print ("Середне" )
+# print (result)
+# 3
+# operation_name3 = "“Перетворення хвилин у години“"
+# print(operation_name3)
+# minutes1 = int(input("Введіть кількість хвилин: "))
+# number_of_minutes1 = minutes1
+# result1 = number_of_minutes1 // 60
+# result2 = number_of_minutes1 % 60
+# print (result1,"год",result2,"хвил")
+# 4
+# operation_name4 = "“Розрахунок знижки”"
+# print(operation_name4)
+# product_price1  = float(input("Цена товара - "))
+# discount1 = float(input("Знижка % - "))
+# result = (product_price1 )-(product_price1*(discount1/100))
+# discount3 = "Сума к розрахунку з знижкою - "
+# print(discount3, result)
+# 5
+# operation_name5 ="“Остання цифра трьохзначного числа”"
+# print(operation_name5)
+# enter_a_number1 = int(input("Введіть число: "))
+# last_digit1 = enter_a_number1 % 10
+# enter_a_number3 = "Остання цифра:"
+# print(enter_a_number3 , last_digit1)
+# 6
+# operation_name5 = "“Периметр прямокутника”"
+# print(operation_name5)
+# length1 = float(input("Довжина : "))
+# width1 = float(input("Ширина  : "))
+# perimeter1 = "Периметр"
+# result = 2*length1 + 2*width1
+# print(perimeter1,result)
+# 7
+# operation_name6 ="“Виведення числа в стовпчик“"
+# print(operation_name6)
+# operation_name7 = int(input("Введить 4-х значне число з клавіатури : "))
+# number = (operation_name7)
+# number1 = number // 1000
+# number2 = number // 100 - number1*10
+# number3 = number // 10 - number1*100 - number2*10
+# number4 = number -number1*1000 - number2*100 - number3*10
+# operation_name8 = "Ваше число в стовпчик : "
+# print(operation_name8)
+# print(number1)
+# print(number2)
+# print(number3)
+# print(number4)
